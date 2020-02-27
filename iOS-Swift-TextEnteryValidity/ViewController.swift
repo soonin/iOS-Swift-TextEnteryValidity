8f53950b31f5f5daad090008d2d9842e7181cd77
Pooya Hatami <am.hatami@gmail.com>
2020-02-26 18:24:34 -0800

    Initial Commit

---
+++ iOS-Swift-TextEnteryValidity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Swift-TextEnteryValidity
+//
+//  Created by Pooya on 2020-02-26.
+//  Copyright © 2020 centurytrail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
